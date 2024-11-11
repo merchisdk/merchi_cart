5f15e32d08e178e1c54500f364ed605587c2182c
James Cool <jamescool@Jamess-MacBook-Pro.local>
2024-11-12 09:48:48 +1100

    extend exports

--- src/index.tsx
+++ src/index.tsx
@@ -10,18 +10,34 @@ import {
   isMerchiCartFetching,
   merchiCartFetchAndSet,
 } from './store';
+import * as components from './components';
+import CardProvider from './CartProvider';
+import CartAlert from './CartAlert';
+import CartHeader from './CartHeader';
+import CartTotals from './CartTotals';
+import * as forms from './forms';
+import * as panels from './panels';
+import * as cartStore from './store';
 
 export {
   ButtonOpenCart,
   ButtonListWrappedOpenCart,
+  cartStore,
   Cart,
+  CartAlert,
+  CartHeader,
+  CardProvider,
+  CartTotals,
+  components,
   doAddCartItem,
   doCartComplete,
   doToggleCartOpen,
+  forms,
   getMerchiCart,
   getMerchiCartValues,
   isMerchiCartFetching,
   merchiCartFetchAndSet,
   MerchiCartModal,
   MerchiShoppingCartModal,
+  panels,
 };
