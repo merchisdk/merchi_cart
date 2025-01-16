34f4e4b39288d93f593a623b3c0e3d1f5f6604ad
James Cool <james@newgenstudio.com>
2025-01-16 11:55:14 +1100

    export ActiveFormShipmentAddressAndNotes

--- src/forms/index.ts
+++ src/forms/index.ts
@@ -1,10 +1,11 @@
 import FormCustomerNew from "./FormNewCustomer";
 import FormReturningCustomer from "./FormReturningCustomer";
-import FormShipmentAddressAndNotes from "./FormShipmentAddressAndNotes";
+import FormShipmentAddressAndNotes, { ActiveFormShipmentAddressAndNotes } from "./FormShipmentAddressAndNotes";
 import FormSquarePayment from "./FormSquarePayment";
 import FormStripePayment from "./FormStripePayment";
 
 export {
+  ActiveFormShipmentAddressAndNotes,
   FormCustomerNew,
   FormReturningCustomer,
   FormShipmentAddressAndNotes,
