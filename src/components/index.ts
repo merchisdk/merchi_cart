6cc2b6eff0b8283666e4ef697f0d805bd8d6f8e7
James Cool <james@newgenstudio.com>
2025-02-03 09:44:10 +1100

    export additional components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,11 +1,14 @@
 import Alert from './Alert';
 import CartBody from './CartBody';
+import CartClient from './CartClient';
 import CartFooter from './CartFooter';
 import CartPaymentSettingsInvalid from './CartPaymentSettingsInvalid';
+import CartShipment from './CartShipment';
 import CartTabContent from './CartTabContent';
 import CartTableContainer from './CartTableContainer';
 import CartTabPanel from './CartTabPanel';
 import Collapse from './Collapse';
+import DiscountInputGroup from './DiscountInputGroup';
 import LoadingTemplate, { LoadingTemplateSm } from './LoadingTemplate';
 import NoCartItems from './NoCartItems';
 import ShipmentGroupCard from './ShipmentGroupCard';
@@ -15,12 +18,15 @@ import Title from './CartTitle';
 export {
   Alert,
   CartBody,
+  CartClient,
   CartFooter,
   CartPaymentSettingsInvalid,
+  CartShipment,
   CartTabContent,
   CartTableContainer,
   CartTabPanel,
   Collapse,
+  DiscountInputGroup,
   LoadingTemplate,
   LoadingTemplateSm,
   NoCartItems,
