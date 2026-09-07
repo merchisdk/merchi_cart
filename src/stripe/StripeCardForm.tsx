bedb9dfe45406445f7c817afe128cfec8ad75c33
Cheng Yu <chengyu0316@gmail.com>
2026-09-08 09:44:38 +1000

    Select original cart card form unless wallet rollout is enabled

--- src/stripe/StripeCardForm.tsx
+++ src/stripe/StripeCardForm.tsx
@@ -1,14 +1,18 @@
 import * as React from 'react';
-import { StripePaymentForm } from 'merchi_invoice';
+import { StripePaymentForm, StripePaymentGate } from 'merchi_invoice';
+import LegacyStripeCardForm from './LegacyStripeCardForm';
 import { useCartContext } from '../CartProvider';
 import { tabIdPaymentSuccess } from '../utilities/tabs';
 
 export default function StripeCardForm() {
   const { alertError, apiUrl, cart, setActiveTabIndex, setInvoiceJson } = useCartContext();
-  return <StripePaymentForm apiUrl={apiUrl || 'https://api.merchi.co/v6/'} resource="cart"
+  return <StripePaymentGate apiUrl={apiUrl || 'https://api.merchi.co/v6/'} resource="cart" resourceId={cart.id}
+    resourceToken={cart.token || cart.cartToken} legacy={<LegacyStripeCardForm />}>
+    <StripePaymentForm apiUrl={apiUrl || 'https://api.merchi.co/v6/'} resource="cart"
     resourceId={cart.id} resourceToken={cart.token || cart.cartToken} onError={alertError}
     onSuccess={invoice => {
       setInvoiceJson(invoice);
       setActiveTabIndex(tabIdPaymentSuccess);
-    }} />;
+    }} />
+  </StripePaymentGate>;
 }
